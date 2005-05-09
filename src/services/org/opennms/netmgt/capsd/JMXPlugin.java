2f64dff4fd07aacce81699ab1d617bad0a9d03c9
Mike Jamison <mjamison@users.sourceforge.net>
2005-05-09 11:43:08 +0000

    The original file was made into an abstract file from which the JBoss and Jsr160 derive from

--- src/services/org/opennms/netmgt/capsd/JMXPlugin.java
+++ src/services/org/opennms/netmgt/capsd/JMXPlugin.java
@@ -1,91 +1,104 @@
-/*
- * Created on Feb 1, 2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
+//
+//This file is part of the OpenNMS(R) Application.
+//
+//OpenNMS(R) is Copyright (C) 2002-2003 The OpenNMS Group, Inc.  All rights reserved.
+//OpenNMS(R) is a derivative work, containing both original code, included code and modified
+//code that was published under the GNU General Public License. Copyrights for modified 
+//and included code are below.
+//
+//OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+//
+//This program is free software; you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation; either version 2 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.                                                            
+//
+//You should have received a copy of the GNU General Public License
+//along with this program; if not, write to the Free Software
+//Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//  
+//For more information contact: 
+// OpenNMS Licensing       <license@opennms.org>
+// http://www.opennms.org/
+// http://www.opennms.com/
+//
 
 package org.opennms.netmgt.capsd;
 
 import java.net.InetAddress;
-import java.util.*;
+import java.util.Map;
 
 import javax.management.MBeanServerConnection;
-import javax.management.remote.JMXConnector;
-import javax.management.remote.JMXConnectorFactory;
-import javax.management.remote.JMXServiceURL;
-import org.opennms.netmgt.utils.ParameterMap;
+
+import org.opennms.protocols.jmx.connectors.ConnectionWrapper;
 
 import org.apache.log4j.Category;
 import org.opennms.core.utils.ThreadCategory;
 
-/**
- * @author mjamison
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
-public class JMXPlugin extends AbstractPlugin {
-    
-    private static final String PROTOCOL_NAME   = "JMX";
-    private static final int    DEFAULT_PORT    = 9004;
-    private static final int    DEFAULT_RETRY   = 0;
-    private static final int    DEFAULT_TIMEOUT = 5000; // in milliseconds
-    
 
-    /* (non-Javadoc)
+/*
+ * This is the base class that handles the actual capability test.  The subclass is responsible 
+ * for getting the ConnectionWrapper that is used to attempt to contact the remote resource.
+ * 
+ * @author <A HREF="mailto:mike@opennms.org">Mike Jamison </A>
+ * @author <A HREF="http://www.opennms.org/">OpenNMS </A>
+ */
+public abstract class JMXPlugin extends AbstractPlugin {
+    
+    private String protocolName = null;
+
+    /* The subclass can use set the protocol name from the parameters map
+     * 
+     * @see org.opennms.netmgt.capsd.Plugin#getProtocolName()
+     */
+    public abstract String getProtocolName(Map parameterMap);
+    
+    /*
+     * The subclass is responsible for getting the connection.
+     */
+    public abstract ConnectionWrapper getMBeanServerConnection(Map parameterMap, InetAddress address);
+    
+    /*
      * @see org.opennms.netmgt.capsd.Plugin#getProtocolName()
      */
     public String getProtocolName() {
-        return PROTOCOL_NAME;
+        return protocolName.toUpperCase();
     }
+
     /* (non-Javadoc)
      * @see org.opennms.netmgt.capsd.Plugin#isProtocolSupported(java.net.InetAddress, java.util.Map)
      */
+
     public boolean isProtocolSupported(InetAddress address, Map map) {
+        
+        if (protocolName == null) {
+            protocolName = getProtocolName(map);
+        }
+
         Category log = ThreadCategory.getInstance(getClass());
-
+        boolean res = false;
+        ConnectionWrapper connection = null;
         try {
-            String namingPort = ParameterMap.getKeyedString(map, "NamingPort",  "9004");
-            String jndiPath   = ParameterMap.getKeyedString(map, "JNDIPath",    "/jmxrmi");
             
-            log.debug("JMX: service:jmx:rmi://" + address.getHostAddress() + "/jndi/rmi://" + address.getHostAddress() + ":" + namingPort + jndiPath);
-            JMXServiceURL url = new JMXServiceURL("service:jmx:rmi://" + address.getHostAddress() + "/jndi/rmi://" + address.getHostAddress() + ":" + namingPort + jndiPath);
-
-            // Connect a JSR 160 JMXConnector to the server side
-            JMXConnector connector = JMXConnectorFactory.connect(url);
-
-            MBeanServerConnection connection = connector.getMBeanServerConnection();
-            Integer value = (Integer)connection.getMBeanCount();
-            log.debug("JMX isProtocolSupported? " + connection + " value: " + value);
-            if (value == null || value.intValue() == 0) {
-                return false;
-            }
+            connection = getMBeanServerConnection(map, address);
             
-            boolean res = connection != null;
-            
-            if (connector != null){
-            	connector.close();
-            }
-            connection = null;
-            
-            return res;
-            
+            Integer result = connection.getMBeanServer().getMBeanCount();
+            log.debug("isProtocolSupported? " + getProtocolName() + " " + result + " " + connection);
+            res = true;
         } catch (Exception e) {
-            log.debug("JMX isProtocolSupported - failed! " + e.getMessage());
-            //e.printStackTrace();
-        } 
+            log.debug(getProtocolName(map) + " - isProtocolSupported - failed! " + e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
         
-        return false;
-    }
-    /* (non-Javadoc)
-     * @see org.opennms.netmgt.capsd.Plugin#isProtocolSupported(java.net.InetAddress)
-     */
-    public boolean isProtocolSupported(InetAddress address) {
-        HashMap map = new HashMap();
-        map.put("NamingPort", "9004");
-        map.put("JNDIPath",   "/jmxrmi");
-        
-        return isProtocolSupported(address, map);
+        return res;
     }
 }
+
