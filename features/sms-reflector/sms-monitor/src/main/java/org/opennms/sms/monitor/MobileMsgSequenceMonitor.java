058be52a6768ee2d52f637917ccb4ac4a7a1e680
Matt Brozowski <brozow@opennms.org>
2010-01-11 13:49:09 -0500

    Refactor MobileMsgSequencer to be injected rather than a static class

--- features/sms-reflector/sms-monitor/src/main/java/org/opennms/sms/monitor/MobileMsgSequenceMonitor.java
+++ features/sms-reflector/sms-monitor/src/main/java/org/opennms/sms/monitor/MobileMsgSequenceMonitor.java
@@ -17,71 +17,80 @@ import org.opennms.sms.monitor.internal.config.MobileSequenceConfig;
 import org.opennms.sms.monitor.internal.config.SequenceConfigFactory;
 import org.opennms.sms.phonebook.Phonebook;
 import org.opennms.sms.phonebook.PhonebookException;
-import org.opennms.sms.phonebook.PropertyPhonebook;
 import org.opennms.sms.ping.PingConstants;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @Distributable(DistributionContext.DAEMON)
 public class MobileMsgSequenceMonitor extends IPv4Monitor {
+
+    private static final String CONTEXT_NAME = "mobileMessagePollerContext";
+
     private static Logger log = Logger.getLogger(MobileMsgSequenceMonitor.class);
-	private Phonebook phonebook = new PropertyPhonebook();
+
+    private Phonebook m_phonebook;
+	private MobileMsgSequencer m_sequencer;
 
 	@Override
 	public void initialize(Map<String,Object> params) {
 		super.initialize(params);
-		BeanUtils.getFactory("mobileMessagePollerContext", ClassPathXmlApplicationContext.class);
+		m_sequencer = BeanUtils.getBean(CONTEXT_NAME, "mobileMsgSequencer", MobileMsgSequencer.class);
+		m_phonebook = BeanUtils.getBean(CONTEXT_NAME, "phonebook", Phonebook.class);
 	}
 
 	@Override
 	public PollStatus poll(MonitoredService svc, Map<String, Object> parameters) {
-		if (parameters.get("retry") == null) {
-			parameters.put("retry", String.valueOf(PingConstants.DEFAULT_RETRIES));
-		}
-		if (parameters.get("timeout") == null) {
-			parameters.put("timeout", String.valueOf(PingConstants.DEFAULT_TIMEOUT));
-		}
-        String config = ParameterMap.getKeyedString(parameters, "sequence", "");
-        if (config == null || "".equals(config)) {
-        	return PollStatus.unavailable("Sequence configuration was empty.  You must specify a 'sequence' parameter in the SMSSequenceMonitor poller configuration!");
-        }
 
-        Properties session = new Properties();
-        try {
-        	// first, transfer anything from the parameters to the session
-			for (Map.Entry<String,Object> entry : parameters.entrySet()) {
-				if (entry.getKey() != null && entry.getValue() != null) {
-					session.put(entry.getKey(), entry.getValue());
-				}
-			}
-			session.setProperty("recipient", phonebook.getTargetForAddress(svc.getIpAddr()));
-		} catch (PhonebookException e) {
-			log.warn("Unable to locate recpient phone number for IP address " + svc.getIpAddr(), e);
-			return PollStatus.unavailable("Unable to find phone number for IP address " + svc.getIpAddr());
-		}
+	    try {
+	        
+	        if (parameters.get("retry") == null) {
+	            parameters.put("retry", String.valueOf(PingConstants.DEFAULT_RETRIES));
+	        }
+	        if (parameters.get("timeout") == null) {
+	            parameters.put("timeout", String.valueOf(PingConstants.DEFAULT_TIMEOUT));
+	        }
+	        String config = ParameterMap.getKeyedString(parameters, "sequence", "");
+	        if (config == null || "".equals(config)) {
+	            return PollStatus.unavailable("Sequence configuration was empty.  You must specify a 'sequence' parameter in the SMSSequenceMonitor poller configuration!");
+	        }
 
-        MobileSequenceConfig sequenceConfig = null;
-        try {
-            SequenceConfigFactory factory = SequenceConfigFactory.getInstance();
-			sequenceConfig = factory.getSequenceForXml(config);
-		} catch (SequenceException e) {
-			log.warn("Unable to parse sequence configuration for host " + svc.getIpAddr(), e);
-			return PollStatus.unavailable("unable to read sequence configuration");
-		}
+	        Properties session = new Properties();
 
-		// FIXME: Decide the validity of an empty sequence; is it a failure to configure?  Or passing because no transactions failed?
-		if (sequenceConfig.getTransactions() == null || sequenceConfig.getTransactions().size() == 0) {
-			log.warn("No transactions were configured for host " + svc.getIpAddr());
-			return PollStatus.unavailable("No transactions were configured for host " + svc.getIpAddr());
-		}
+	        // first, transfer anything from the parameters to the session
+	        for (Map.Entry<String,Object> entry : parameters.entrySet()) {
+	            if (entry.getKey() != null && entry.getValue() != null) {
+	                session.put(entry.getKey(), entry.getValue());
+	            }
+	        }
+	        session.setProperty("recipient", m_phonebook.getTargetForAddress(svc.getIpAddr()));
 
-		try {
-			Map<String, Number> responseTimes = MobileMsgSequencer.executeSequence(sequenceConfig, session);
-			PollStatus response = PollStatus.available();
-			response.setProperties(responseTimes);
-			return response;
-		} catch (Throwable e) {
-			log.debug("Sequence failed", e);
-			return PollStatus.unavailable("Sequence failed: " + e.getLocalizedMessage());
-		}
+
+	        MobileSequenceConfig sequenceConfig = null;
+
+	        SequenceConfigFactory factory = SequenceConfigFactory.getInstance();
+	        sequenceConfig = factory.getSequenceForXml(config);
+
+
+	        // FIXME: Decide the validity of an empty sequence; is it a failure to configure?  Or passing because no transactions failed?
+	        if (sequenceConfig.getTransactions() == null || sequenceConfig.getTransactions().size() == 0) {
+	            log.warn("No transactions were configured for host " + svc.getIpAddr());
+	            return PollStatus.unavailable("No transactions were configured for host " + svc.getIpAddr());
+	        }
+
+
+	        Map<String, Number> responseTimes = m_sequencer.executeSequence(sequenceConfig, session);
+	        PollStatus response = PollStatus.available();
+	        response.setProperties(responseTimes);
+	        return response;
+
+
+	    } catch (PhonebookException e) {
+	        log.warn("Unable to locate recpient phone number for IP address " + svc.getIpAddr(), e);
+	        return PollStatus.unavailable("Unable to find phone number for IP address " + svc.getIpAddr());
+	    } catch (SequenceException e) {
+	        log.warn("Unable to parse sequence configuration for host " + svc.getIpAddr(), e);
+	        return PollStatus.unavailable("unable to read sequence configuration");
+	    } catch (Throwable e) {
+	        log.debug("Sequence failed", e);
+	        return PollStatus.unavailable("Sequence failed: " + e.getLocalizedMessage());
+	    } 
 	}
 }
