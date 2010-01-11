dc857f475d6eb0fa81dc25630473304f2b5c73f5
Matt Brozowski <brozow@opennms.org>
2010-01-11 13:49:09 -0500

    Refactor MobileMsgSequencer to be injected rather than a static class

--- features/sms-reflector/sms-monitor/src/main/java/org/opennms/sms/monitor/MobileMsgSequencer.java
+++ features/sms-reflector/sms-monitor/src/main/java/org/opennms/sms/monitor/MobileMsgSequencer.java
@@ -3,43 +3,34 @@
  */
 package org.opennms.sms.monitor;
 
-import static org.opennms.core.utils.LogUtils.debugf;
-
 import java.util.Map;
 import java.util.Properties;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.opennms.core.tasks.DefaultTaskCoordinator;
-import org.opennms.core.utils.PropertiesUtils;
 import org.opennms.sms.monitor.internal.config.MobileSequenceConfig;
 import org.opennms.sms.reflector.smsservice.MobileMsgTracker;
 
 public class MobileMsgSequencer {
-	private static MobileMsgTracker s_tracker;
-	private static DefaultTaskCoordinator s_coordinator;
-	private static boolean m_initialized = false;
 
-	public synchronized static void initialize() {
-		if (!m_initialized) {
-			ExecutorService executor = Executors.newSingleThreadExecutor();
-			s_coordinator = new DefaultTaskCoordinator(executor);
-		    if (s_tracker == null) {
-		    	throw new IllegalStateException("MobileMsgSequencer not yet initialized!!"); 
-		    }
-		    m_initialized = true;
-		}
+	private DefaultTaskCoordinator m_coordinator;
+	private MobileMsgTracker m_tracker;
+
+	public MobileMsgSequencer() {
+	    m_coordinator = new DefaultTaskCoordinator(Executors.newSingleThreadExecutor());
 	}
-
-	public synchronized static void setMobileMsgTracker(MobileMsgTracker tracker) {
-	    debugf(MobileMsgSequencer.class, "Initializing MobileMsgSequencer with tracker %s", tracker);
-	    s_tracker = tracker;
-	}
-
 	
-	public static Map<String,Number> executeSequence(MobileSequenceConfig sequenceConfig, Properties session) throws Throwable {
-		initialize();
-        return new MobileMsgSequenceBuilder(sequenceConfig).execute(session, s_tracker, s_coordinator);
+	public void setMobileMsgTracker(MobileMsgTracker tracker) {
+	    m_tracker = tracker;
 	}
+	
+	public void setTaskCoordinator(DefaultTaskCoordinator coordinator) {
+	    m_coordinator = coordinator;
+	}
+	
+	public Map<String, Number> executeSequence(MobileSequenceConfig sequenceConfig, Properties session) throws Throwable {
+        return new MobileMsgSequenceBuilder(sequenceConfig).execute(session, m_tracker, m_coordinator);
+	}
+
 
 }
