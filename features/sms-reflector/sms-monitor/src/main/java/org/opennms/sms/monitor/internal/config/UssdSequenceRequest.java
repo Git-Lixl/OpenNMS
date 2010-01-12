7842e39464b26e3b0188d0745ac84c9275307db9
Donald Desloge <desloge@opennms.com>
2010-01-12 17:57:07 -0500

    Beginning of refactoring the MMTM

--- features/sms-reflector/sms-monitor/src/main/java/org/opennms/sms/monitor/internal/config/UssdSequenceRequest.java
+++ features/sms-reflector/sms-monitor/src/main/java/org/opennms/sms/monitor/internal/config/UssdSequenceRequest.java
@@ -1,6 +1,15 @@
 package org.opennms.sms.monitor.internal.config;
 
+import java.util.Properties;
+
 import javax.xml.bind.annotation.XmlRootElement;
+
+import org.opennms.core.utils.PropertiesUtils;
+import org.opennms.sms.monitor.SequencerException;
+import org.opennms.sms.monitor.MobileMsgSequenceBuilder.MobileMsgTransactionBuilder;
+import org.opennms.sms.monitor.MobileMsgSequenceBuilder.SmsTransactionBuilder;
+import org.opennms.sms.monitor.MobileMsgSequenceBuilder.UssdTransactionBuilder;
+import org.opennms.sms.reflector.smsservice.MobileMsgSequence;
 
 @XmlRootElement(name="ussd-request")
 public class UssdSequenceRequest extends MobileSequenceRequest {
@@ -15,6 +24,17 @@ public class UssdSequenceRequest extends MobileSequenceRequest {
 
 	public UssdSequenceRequest(String gatewayId, String label, String text) {
 		super(gatewayId, label, text);
+	}
+
+	@Override
+	public MobileMsgTransactionBuilder getRequestTransaction(MobileMsgSequence sequence, Properties session, String defaultLabel,
+			String defaultGatewayId, long defaultTimeout, int defaultRetries) throws SequencerException {
+		
+		String label1 = PropertiesUtils.substitute(getLabel() == null ? defaultLabel : getLabel(), session);
+		String gatewayId = PropertiesUtils.substitute(getGatewayId(), session);
+		String text = PropertiesUtils.substitute(getText(), session);
+		
+		return new UssdTransactionBuilder(this, sequence, label1, gatewayId == null? defaultGatewayId : gatewayId, defaultTimeout, defaultRetries, text);
 	}
 
 }
--- features/sms-reflector/sms-monitor/src/main/java/org/opennms/sms/monitor/internal/config/UssdSequenceResponse.java
+++ features/sms-reflector/sms-monitor/src/main/java/org/opennms/sms/monitor/internal/config/UssdSequenceResponse.java
@@ -1,6 +1,11 @@
 package org.opennms.sms.monitor.internal.config;
 
+import static org.opennms.sms.reflector.smsservice.MobileMsgResponseMatchers.isSms;
+import static org.opennms.sms.reflector.smsservice.MobileMsgResponseMatchers.isUssd;
+
 import javax.xml.bind.annotation.XmlRootElement;
+
+import org.opennms.sms.reflector.smsservice.MobileMsgResponseMatcher;
 
 @XmlRootElement(name="ussd-response")
 public class UssdSequenceResponse extends MobileSequenceResponse {
@@ -15,5 +20,9 @@ public class UssdSequenceResponse extends MobileSequenceResponse {
 	
 	public UssdSequenceResponse(String gatewayId, String label) {
 		super(gatewayId, label);
+	}
+
+	protected MobileMsgResponseMatcher getResponseTypeMatcher() {
+		return isUssd();
 	}
 }
