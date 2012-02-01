5dc867a4085693f967d90cdbc2b57b5f599c1cf1
Ronny Trommer <ronny@opennms.org>
2012-02-01 15:23:42 +0100

    Puppet requisition url handler created. Not finished yet :)

--- integrations/opennms-puppet-url-requisition/src/main/java/org/opennms/netmgt/provision/service/puppet/PuppetRequisitionUrlConnection.java
+++ integrations/opennms-puppet-url-requisition/src/main/java/org/opennms/netmgt/provision/service/puppet/PuppetRequisitionUrlConnection.java
@@ -1,36 +1,59 @@
 package org.opennms.netmgt.provision.service.puppet;
 
+import org.apache.commons.io.IOExceptionWithCause;
 import org.opennms.core.utils.url.GenericURLConnection;
+import org.opennms.core.xml.JaxbUtils;
 import org.opennms.netmgt.provision.persist.requisition.Requisition;
+import org.opennms.netmgt.provision.persist.requisition.RequisitionInterface;
 import org.opennms.netmgt.provision.persist.requisition.RequisitionNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.xml.bind.JAXBException;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.InputStream;
+import java.net.*;
 import java.util.Map;
-
 /**
- * <p>LegacyLocalReportsDaoTest class.</p>
+ * <p>PuppetRequisitionUrlConnection class.</p>
  *
  * @author Ronny Trommer <ronny@opennms.org>
  * @version $Id: $
- * @since 1.8.1
+ * @since 1.10.1
  */
 public class PuppetRequisitionUrlConnection extends GenericURLConnection {
-    
-    private String m_hostname;
-    
-    private String m_username;
-    
-    private String m_password;
+    private Logger logger = LoggerFactory.getLogger(PuppetRequisitionUrlConnection.class);
 
     private static Map<String, String> m_args;
 
+    private Requisition m_requisition = null;
+
+    private PuppetRestClient m_puppetRestClient;
+    
+    private String m_host;
+    
+    private Integer m_port;
+    
+    private URL m_puppetRestUrl;
+
     protected PuppetRequisitionUrlConnection(URL url) throws MalformedURLException {
         super(url);
-        m_username = getUsername(url);
-        m_password = getPassword(url);
+        
+        m_host = url.getHost();
+        
+        m_port = url.getPort();
+
         m_args = getQueryArgs(url);
+        
+        m_puppetRestUrl = new URL("https://" + m_host + ":" + m_port);
+
+        try {
+            // https://{puppetmaster}:8140
+            m_puppetRestClient = new PuppetRestClient(m_puppetRestUrl);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
     }
 
     /**
@@ -44,11 +67,76 @@ public class PuppetRequisitionUrlConnection extends GenericURLConnection {
         // We do nothing here, cause we do not really open a connection
     }
 
+    private Requisition buildPuppetRequisition() {
+        return new Requisition();
+    }
+
     private RequisitionNode createRequisitionNode(String puppetNodeRecord) {
+        RequisitionNode requisitionNode = new RequisitionNode();
+
+        RequisitionInterface requisitionInterface = new RequisitionInterface();
+
+        //puppetNodeRecord -> Puppet node name: itchy.opennms-edu.net
+        m_puppetRestClient.getFactsByPuppetNode(puppetNodeRecord);
+
+        /*
+         * node label:
+         * ip address:
+         * uuid
+         * operatingsystem:
+         */
+
+        // Setting the node label
+        requisitionNode.setNodeLabel("<nodelabel>");
+
+        // Foreign Id to map against uuid from puppetmaster
+        requisitionNode.setForeignId("<puppet-uuid>");
+
+        // Primary interface
+        boolean primary = true;
+
+        try {
+            InetAddress inetAddress = InetAddress.getByName("");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        requisitionNode.putInterface(requisitionInterface);
+
         return new RequisitionNode();
     }
 
-    private Requisition buildRequisitionFromZoneTransfer() {
-        return new Requisition();
+    /**
+     * {@inheritDoc}
+     * <p/>
+     * Creates a ByteArrayInputStream implementation of InputStream of the XML
+     * marshaled version of the Requisition class. Calling close on this stream
+     * is safe.
+     */
+    @Override
+    public InputStream getInputStream() throws IOException {
+
+        InputStream stream = null;
+
+        try {
+            Requisition r = buildPuppetRequisition();
+            stream = new ByteArrayInputStream(jaxBMarshal(r).getBytes());
+        } catch (Throwable e) {
+            logger.warn("Problem getting input stream: '{}'", e);
+            throw new IOExceptionWithCause("Problem getting input stream: " + e, e);
+        }
+
+        return stream;
+    }
+
+    /**
+     * Utility to marshal the Requisition class into XML.
+     *
+     * @param r
+     * @return a String of XML encoding the Requisition class
+     * @throws javax.xml.bind.JAXBException
+     */
+    private String jaxBMarshal(Requisition r) throws JAXBException {
+        return JaxbUtils.marshal(r);
     }
 }
---
+++ integrations/opennms-puppet-url-requisition/src/main/java/org/opennms/netmgt/provision/service/puppet/PuppetRestClient.java
@@ -0,0 +1,93 @@
+package org.opennms.netmgt.provision.service.puppet;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <p>PuppetRestClient class.</p>
+ *
+ * @author Ronny Trommer <ronny@opennms.org>
+ * @version $Id: $
+ * @since 1.10.1
+ */
+public class PuppetRestClient {
+    private Logger logger = LoggerFactory.getLogger(PuppetRequisitionUrlConnection.class);
+    /**
+     * HTTP client for ReST connection
+     */
+    private Client m_client;
+
+    /**
+     * HTTP client connection configuration
+     */
+    private DefaultClientConfig m_clientConfig;
+
+    /**
+     * Data structure from ReST call
+     */
+    private WebResource m_webResource;
+
+    /**
+     * Media type for ReST request to Puppet master
+     */
+    private final String MEDIA_TYPE_YAML = "yaml";
+
+    /**
+     * Default constructor with URL for ReST request
+     *
+     * @param url a {@link }
+     * @throws URISyntaxException
+     */
+    public PuppetRestClient(URL url) throws URISyntaxException {
+        m_clientConfig = new DefaultClientConfig();
+        m_client = Client.create(m_clientConfig);
+
+        logger.debug("Initialize Puppet ReST client with URL: '{}'", url.toString());
+        m_webResource = m_client.resource(url.toURI());
+    }
+    
+    public List<String> getPuppetNodeByFactsSearch(String environment, String search) {
+        Yaml puppetNodeListYaml = new Yaml();
+        ArrayList<String> puppetHosts = new ArrayList<String>();
+        
+        // https://{puppetmaster}:8140/{environment}/facts_search/search?facts.operatingsystem=Ubuntu
+        // search = facts.operatingsystem=Ubuntu
+        String puppetSearchResult = m_webResource.path(environment).path("facts_search").path("search?" + search).accept(MEDIA_TYPE_YAML).get(String.class);
+        logger.debug("Search result for puppet nodes: '{}'", puppetSearchResult);
+
+        puppetHosts = (ArrayList<String>) puppetNodeListYaml.load(puppetSearchResult);
+        return puppetHosts;
+    }
+
+    public Map<String,String> getFactsByPuppetNode(String puppetNode) {
+        Map<String,String> nodeFacts = new HashMap<String,String>();
+
+        //https://{puppetmaster}:8140/production/node/patches.opennms-edu.net
+        String puppetNodeYaml = m_webResource.path("production").path("node").path(puppetNode).accept(MEDIA_TYPE_YAML).get(String.class).replace("!ruby/", "");
+        logger.debug("Get puppet node facts for node '{}': '{}'", puppetNode, puppetNodeYaml);
+
+        try {
+            ArrayList<String> lines = (ArrayList<String>) IOUtils.readLines(new StringReader(puppetNodeYaml));
+            for (String line : lines) {
+                nodeFacts.put(line.split(":")[0].trim(), line.split(":")[1].trim().replaceAll("\"", ""));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return nodeFacts;
+    }
+}
