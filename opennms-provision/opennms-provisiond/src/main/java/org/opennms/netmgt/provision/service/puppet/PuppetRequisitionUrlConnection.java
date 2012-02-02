65fe31a6943b4768602ca4ea5bf696e51d2a12be
Ronny Trommer <ronny@opennms.org>
2012-02-02 16:04:44 +0100

    Added boiler plates javadoc and removed bean mapping utilities for annotations, cause illegal access exception

--- opennms-provision/opennms-provisiond/src/main/java/org/opennms/netmgt/provision/service/puppet/PuppetRequisitionUrlConnection.java
+++ opennms-provision/opennms-provisiond/src/main/java/org/opennms/netmgt/provision/service/puppet/PuppetRequisitionUrlConnection.java
@@ -1,3 +1,31 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2010-2011 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2011 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
 package org.opennms.netmgt.provision.service.puppet;
 
 import org.apache.commons.io.IOExceptionWithCause;
@@ -5,10 +33,9 @@ import org.apache.commons.lang.StringUtils;
 import org.opennms.core.utils.url.GenericURLConnection;
 import org.opennms.core.xml.JaxbUtils;
 import org.opennms.netmgt.provision.persist.requisition.Requisition;
+import org.opennms.netmgt.provision.persist.requisition.RequisitionAsset;
 import org.opennms.netmgt.provision.persist.requisition.RequisitionInterface;
 import org.opennms.netmgt.provision.persist.requisition.RequisitionNode;
-import org.opennms.netmgt.provision.service.puppet.tools.Map2BeanUtils;
-import org.opennms.netmgt.provision.service.puppet.tools.RequisitionAssetUtils;
 import org.opennms.netmgt.provision.service.puppet.tools.SSLUtilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,10 +44,10 @@ import javax.xml.bind.JAXBException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.InvocationTargetException;
 import java.net.*;
 import java.util.List;
 import java.util.Map;
+
 /**
  * <p>PuppetRequisitionUrlConnection class.</p>
  *
@@ -30,46 +57,77 @@ import java.util.Map;
  */
 public class PuppetRequisitionUrlConnection extends GenericURLConnection {
 
+    /**
+     * Log to OPENNMS_HOME/logs/daemon/provisiond.log
+     */
     private Logger logger = LoggerFactory.getLogger("Provisiond." + PuppetRequisitionUrlConnection.class.getName());
 
+    /**
+     * Arguments from provisionds import url resource
+     */
     private static Map<String, String> m_args;
 
+    /**
+     * Puppet ReST client implementation
+     */
     private PuppetRestClient m_puppetRestClient;
-    
+
+    /**
+     * Host name of puppet master server
+     */
     private String m_host;
-    
+
+    /**
+     * Port from puppet masters ReST API (default: 8140)
+     */
     private Integer m_port;
-    
+
+    /**
+     * Puppet masters ReST base URL
+     */
     private URL m_puppetRestUrl;
 
+    /**
+     * OpenNMS foreign source for the import
+     */
     private String m_foreignSource;
-    
+
+    /**
+     * Puppet environment
+     */
     private String m_puppetEnvironment;
 
+    /**
+     * Constructor for the URL handling
+     *
+     * @param url
+     * @throws MalformedURLException
+     */
     public PuppetRequisitionUrlConnection(URL url) throws MalformedURLException {
         super(url);
 
+        // TODO indigo: For security reasons, please import your puppet masters SSL certificate in your JVM. Only for test environment
+        // Figure out how we can handle this in a safe way even you run massively parallel. THIS IS A JVM SETTING!
         SSLUtilities.trustAllHostnames();
         SSLUtilities.trustAllHttpsCertificates();
-        
+
         m_host = url.getHost();
-        logger.debug("Init puppet requisition host '{}'", m_host);
-        
+
         m_port = url.getPort();
-        logger.debug("Init puppet requisition port '{}'", m_port);
-        
+
         m_args = getQueryArgs(url);
-        
+
         m_puppetRestUrl = new URL("https://" + m_host + ":" + m_port);
-        logger.debug("Init puppet ReST URL '{}'", m_puppetRestUrl);
 
         m_foreignSource = parseForeignSource(url);
-        logger.debug("Init puppet foreign source '{}'", m_foreignSource);
-                
-        m_puppetEnvironment = parsePuppetEnvironment(url);
-        logger.debug("Init puppet environment '{}'", m_puppetEnvironment);
 
-        logger.debug("Initialized PuppetRequisitionUrlConnection with URL: '{}' and args: '{}'", url, m_args);
+        m_puppetEnvironment = parsePuppetEnvironment(url);
+
+        logger.debug("Initialize puppet requisition url connection: '{}'", "Host[" + m_host +
+                "] Environment[" + m_puppetEnvironment +
+                "] Foreign source[" + m_foreignSource +
+                "] ReST URL[" + m_puppetRestUrl +
+                "] Arguments[" + m_args);
     }
 
     /**
@@ -80,46 +138,66 @@ public class PuppetRequisitionUrlConnection extends GenericURLConnection {
      */
     @Override
     public void connect() throws IOException {
-        // We do nothing here, cause we do not really open a connection
+        // No op
     }
 
+    /**
+     * <p>buildPuppetRequisition</p>
+     * 
+     * Build the OpenNMS requisition data structure from puppet master.
+     * 
+     * @return a {@link org.opennms.netmgt.provision.persist.requisition.Requisition} object
+     */
     private Requisition buildPuppetRequisition() {
         Requisition requisition = new Requisition(m_foreignSource);
-        
+
         try {
             // https://{puppetmaster}:8140
             m_puppetRestClient = new PuppetRestClient(m_puppetRestUrl);
-            List<String> puppetNodeList = m_puppetRestClient.getPuppetNodesByFactsSearch(m_puppetEnvironment,"facts.operatingsystem=Ubuntu");
-        
+            List<String> puppetNodeList = m_puppetRestClient.getPuppetNodesByFactsSearch(m_puppetEnvironment, "facts.operatingsystem=Ubuntu");
+
             for (String puppetNode : puppetNodeList) {
                 requisition.insertNode(createRequisitionNode(puppetNode));
                 logger.debug("Insert puppet requisition node '{}' from environment '{}'", puppetNode, m_puppetEnvironment);
-             }
+            }
         } catch (URISyntaxException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
-        
+
         return requisition;
     }
 
+    /**
+     * Create a specific OpenNMS requisition node from puppet master identified by the puppet node name. Retrieve puppet
+     * facts and fill up the OpenNMS requisition node model.
+     * 
+     * @param puppetNode a {@link java.lang.String} puppet node name
+     * @return a {@link org.opennms.netmgt.provision.persist.requisition.RequisitionNode} object
+     */
     private RequisitionNode createRequisitionNode(String puppetNode) {
         logger.debug("Create requisition node for puppet node '{}'", puppetNode);
+
+        // Initialize a node
         RequisitionNode requisitionNode = new RequisitionNode();
 
+        // Initialize a interface
         RequisitionInterface requisitionInterface = new RequisitionInterface();
 
-        //environnment -> Puppet environment: production
-        //puppetNode -> Puppet node name: itchy.opennms-edu.net
-        Map<String,String> puppetNodeFacts = m_puppetRestClient.getFactsByPuppetNode(m_puppetEnvironment, puppetNode);
+        // environment -> Puppet environment: production
+        // puppetNode -> Puppet node name: itchy.opennms-edu.net
+        Map<String, String> puppetNodeFacts = m_puppetRestClient.getFactsByPuppetNode(m_puppetEnvironment, puppetNode);
 
-        // Setting the node label
+        // Set OpenNMS node label
         requisitionNode.setNodeLabel(puppetNode);
         logger.debug("Set node label: '{}'", puppetNode);
 
-        // Foreign Id to map against uuid from puppetmaster
+        // Foreign Id as unique ide in puppet master
+        // TODO indigo: We have to be sure "uniqueid" is really unique in puppet
         requisitionNode.setForeignId(puppetNodeFacts.get("uniqueid"));
         logger.debug("Set node foreign ID: '{}'", puppetNodeFacts.get("uniqueid"));
 
+        // Verify IP addresses an initialize the interface model
+        // TODO indigo: We have to figure out what happen with more then one ip interface and IPv6?
         try {
             InetAddress inetAddress = InetAddress.getByName(puppetNodeFacts.get("ipaddress"));
             requisitionInterface.setIpAddr(puppetNodeFacts.get("ipaddress"));
@@ -127,19 +205,22 @@ public class PuppetRequisitionUrlConnection extends GenericURLConnection {
         } catch (UnknownHostException e) {
             logger.error("Error parsing IP address '{}'. Error message: '{}'", puppetNodeFacts.get("ipaddress"), e.getMessage());
         }
-        
-        PuppetModel puppetAssetModel = new PuppetModel();
-        try {
-            puppetAssetModel = (PuppetModel) Map2BeanUtils.fill(puppetAssetModel,puppetNodeFacts);
-            requisitionNode.setAssets(RequisitionAssetUtils.generateRequisitionAssets(puppetAssetModel));
-        } catch (IllegalAccessException e) {
-            logger.error("Illegal access ocured. Error message: '{}'", e.getMessage());
-        } catch (InvocationTargetException e) {
-            logger.error("Invocation target exception. Error message: '{}'", e.getMessage());
-        } catch (NoSuchMethodException e) {
-            logger.error("No such method exception. Error message: '{}'", e.getMessage());
-        }
 
+        // Assign puppet facts to OpenNMS sssets
+        RequisitionAsset manufacturerAsset = new RequisitionAsset("manufacturer", puppetNodeFacts.containsKey("manufacturer") ? puppetNodeFacts.get("manufacturer") : "");
+        RequisitionAsset osAsset = new RequisitionAsset("operatingSystem", puppetNodeFacts.containsKey("lsbdistdescription") ? puppetNodeFacts.get("lsbdistdescription") : "");
+        RequisitionAsset serialnumberAsset = new RequisitionAsset("serialNumber", puppetNodeFacts.containsKey("serialnumber") ? puppetNodeFacts.get("serialnumber") : "");
+        RequisitionAsset modelnumberAsset = new RequisitionAsset("modelNumber", puppetNodeFacts.containsKey("productname") ? puppetNodeFacts.get("productname") : "");
+        RequisitionAsset cpuAsset = new RequisitionAsset("cpu", puppetNodeFacts.containsKey("processor0") ? puppetNodeFacts.get("processor0") : "");
+
+        // Add assets to requisistion node
+        requisitionNode.putAsset(manufacturerAsset);
+        requisitionNode.putAsset(osAsset);
+        requisitionNode.putAsset(serialnumberAsset);
+        requisitionNode.putAsset(modelnumberAsset);
+        requisitionNode.putAsset(cpuAsset);
+
+        // Configure the interface and initialize
         requisitionInterface.setManaged(Boolean.TRUE);
         requisitionInterface.setStatus(Integer.valueOf(1));
         requisitionNode.putInterface(requisitionInterface);
@@ -150,8 +231,8 @@ public class PuppetRequisitionUrlConnection extends GenericURLConnection {
     /**
      * Foreign Source should be the second path entity, if it exists, otherwise it is
      * set to the value of the zone.
-     *
-     *   dns://<host>/<zone>[/<foreign source>][/<?expression=<regex>>
+     * <p/>
+     * puppet://<host>/<environment>[/<foreign source>][/<?expression=<regex>>
      *
      * @param url a {@link java.net.URL} object.
      * @return a {@link java.lang.String} object.
@@ -175,8 +256,8 @@ public class PuppetRequisitionUrlConnection extends GenericURLConnection {
 
     /**
      * Puppet environment should be the first path entity
-     *
-     *   puppet://<host>/<puppet environment>[/<foreign source>][/<?expression=<regex>>
+     * <p/>
+     * puppet://<host>/<puppet environment>[/<foreign source>][/<?expression=<regex>>
      *
      * @param url a {@link java.net.URL} object.
      * @return a {@link java.lang.String} object.
--- opennms-provision/opennms-provisiond/src/main/java/org/opennms/netmgt/provision/service/puppet/PuppetRestClient.java
+++ opennms-provision/opennms-provisiond/src/main/java/org/opennms/netmgt/provision/service/puppet/PuppetRestClient.java
@@ -1,3 +1,31 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2010-2011 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2011 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
 package org.opennms.netmgt.provision.service.puppet;
 
 import com.sun.jersey.api.client.Client;
@@ -49,7 +77,7 @@ public class PuppetRestClient {
     /**
      * Default constructor with URL for ReST request
      *
-     * @param url a {@link }
+     * @param url a {@link java.net.URL} puppet master ReST URL
      * @throws URISyntaxException
      */
     public PuppetRestClient(URL url) throws URISyntaxException {
@@ -59,7 +87,16 @@ public class PuppetRestClient {
         logger.debug("Initialize Puppet ReST client with URL: '{}'", url.toString());
         m_webResource = m_client.resource(url.toURI());
     }
-    
+
+    /**
+     * <p>getPuppetNodesByFactsSearch</p>
+     * 
+     * Get puppet nodes specified by facts search
+     * 
+     * @param environment a {@link java.lang.String} puppet environment
+     * @param search a {@link java.lang.String} puppet fact search expression
+     * @return puppetHosts a {@link java.lang.String} list with puppet node names
+     */
     public List<String> getPuppetNodesByFactsSearch(String environment, String search) {
         Yaml puppetNodeListYaml = new Yaml();
         ArrayList<String> puppetHosts = new ArrayList<String>();
@@ -74,6 +111,15 @@ public class PuppetRestClient {
         return puppetHosts;
     }
 
+    /**
+     * <p>getFactsByPuppetNode</p>
+     * 
+     * Get all facts from a specific puppet node by name
+     * 
+     * @param environment a {@link java.lang.String} puppet environment
+     * @param puppetNode a {@link java.lang.String} puppet node name
+     * @return nodeFacts a {@link java.util.Map} map with node facts
+     */
     public Map<String,String> getFactsByPuppetNode(String environment, String puppetNode) {
         Map<String,String> nodeFacts = new HashMap<String,String>();
 
