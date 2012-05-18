2aef22e7b1c00ead36ca0699470c78be3cbb624b
Ronny Trommer <ronny@opennms.org>
2012-05-18 18:37:47 +0200

    Move alarmDetail.jsp to Spring MVC for alarm detail page. Create AlarmDetailController and first draft for the alarm detail view.

---
+++ opennms-webapp/src/main/java/org/opennms/web/controller/alarm/AlarmDetailController.java
@@ -0,0 +1,108 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2010-2011 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2011 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.web.controller.alarm;
+
+import org.opennms.web.alarm.Alarm;
+import org.opennms.web.alarm.WebAlarmRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.Assert;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * <p>AlarmDetailController class.</p>
+ *
+ * @author Ronny Trommer <ronny@opennms.org>
+ * @version $Id: $
+ * @since 1.8.1
+ */
+public class AlarmDetailController extends AbstractController {
+
+    /**
+     * OpenNMS alarm repository
+     */
+    private WebAlarmRepository m_webAlarmRepository;
+
+    /**
+     * Alarm to display
+     */
+    private Alarm m_alarm;
+
+    /**
+     * Logging
+     */
+    private Logger logger = LoggerFactory.getLogger("OpenNMS.WEB." + AlarmDetailController.class.getName());
+
+    /**
+     * <p>setWebAlarmRepository</p>
+     *
+     * @param webAlarmRepository a {@link org.opennms.web.alarm.WebAlarmRepository} object.
+     */
+    public void setWebAlarmRepository(WebAlarmRepository webAlarmRepository) {
+        m_webAlarmRepository = webAlarmRepository;
+    }
+
+    /**
+     * <p>afterPropertiesSet</p>
+     *
+     * @throws java.lang.Exception if any.
+     */
+    public void afterPropertiesSet() throws Exception {
+        Assert.notNull(m_webAlarmRepository, "webAlarmRepository must be set");
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * Display alarm detail page
+     */
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        int alarmId;
+        String alarmIdString = "";
+
+        try {
+            alarmIdString = httpServletRequest.getParameter("id");
+            alarmId = Integer.parseInt(alarmIdString);
+
+            m_alarm = m_webAlarmRepository.getAlarm(alarmId);
+            logger.debug("Alarm retrieved: '{}'", m_alarm.toString());
+        } catch (NumberFormatException e) {
+            logger.error("Could not parse alarm ID '{}' to integer.", httpServletRequest.getParameter("id"));
+        } catch (Exception e) {
+            logger.error("Could not retrieve alarm from webAlarmRepository for ID='{}'", alarmIdString);
+        }
+
+        return new ModelAndView("alarm/detail.htm", "alarmDetail", m_alarm);
+    }
+}
