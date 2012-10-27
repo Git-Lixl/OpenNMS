9506d6cc2cc283174e6795755b4571718fd98e7c
Antonio <rssntn67@yahoo.it>
2012-10-27 16:21:03 +0200

    Cleaned config file from default used value for tests

--- integrations/opennms-integration-remedy/src/main/java/org/opennms/netmgt/ticketer/remedy/DefaultRemedyConfigDao.java
+++ integrations/opennms-integration-remedy/src/main/java/org/opennms/netmgt/ticketer/remedy/DefaultRemedyConfigDao.java
@@ -50,48 +50,39 @@ public class DefaultRemedyConfigDao {
 	 */
 	public String getUserName() {
 		return getProperties().getString("remedy.username");
-		//return "opennmstnn";
 	}
 
 	
 	String getPassword() {
 		return getProperties().getString("remedy.password");
-		//return "TNNwsC4ll";
 	}
 
 	String getAuthentication() {
 		return getProperties().getString("remedy.authentication");
-		//return "ARSystem";
 	}
 
 	String getLocale() {
 		return getProperties().getString("remedy.locale");		
-		//return "it_IT";
 	}
 	
 	String getTimeZone() {
 		return getProperties().getString("remedy.timezone");
-		//return "CET";
 	}
 	
 	String getEndPoint() {
 		return getProperties().getString("remedy.endpoint");
-		//return "http://172.20.0.76:8180/arsys/services/ARService?server=itts3h&webService=HPD_IncidentInterface_WS";
 	}
 
 	String getPortName() {
 		return getProperties().getString("remedy.portname");
-		//return "HPD_IncidentInterface_WSPortTypeSoap";
 	}
 
 	String getCreateEndPoint() {
 		return getProperties().getString("remedy.createendpoint");
-		//return "http://172.20.0.76:8180/arsys/services/ARService?server=itts3h&webService=HPD_IncidentInterface_Create_WS";
 	}
 
 	String getCreatePortName() {
 		return getProperties().getString("remedy.createportname");
-		//return "HPD_IncidentInterface_Create_WSPortTypeSoap";
 	}
 
 	List<String> getTargetGroups() {
@@ -104,93 +95,73 @@ public class DefaultRemedyConfigDao {
 	
 	String getAssignedGroup() {
 		return getProperties().getString("remedy.assignedgroup");
-		//return "TNnet";
 	}
 
 	String getFirstName() {
 		return getProperties().getString("remedy.firstname");
-		//return "Opennms";
 	}
 
 	String getLastName() {
 		return getProperties().getString("remedy.lastname");
-		//return "Tnn";
 	}
 
 	String getServiceCI() {
 		return getProperties().getString("remedy.serviceCI");
-		//return "Trentino Network Event Management [I.TNEVT]";
-		//return "Trentino Network Connettivit� [C.TNNCN]";
 	}
 
 	String getServiceCIReconID() {
 		return getProperties().getString("remedy.serviceCIReconID");
-		//return "RE00505688005eP8Z3UAsLJIHg6EoR";
-		//return "RE00505688005e3s-nTg4KEI5gFSov";
 	}
 		
 	String getAssignedSupportCompany() {
 		return getProperties().getString("remedy.assignedsupportcompany");
-		//return "Trentino Network srl";
 	}
 
 	String getAssignedSupportOrganization() {
 		return getProperties().getString("remedy.assignedsupportorganization");
-		//return "Centro Gestione Rete";
 	}
 
 	String getCategorizationtier1() {
 		return getProperties().getString("remedy.categorizationtier1");
-		//return "Incident";
 	}
 
 	String getCategorizationtier2() {
 		return getProperties().getString("remedy.categorizationtier2");
-		//return "Generic";
 	}
 
 	String getCategorizationtier3() {
 		return getProperties().getString("remedy.categorizationtier3");
-		//return "Non bloccante";
 	}
 	
 	String getServiceType() {
 		return getProperties().getString("remedy.serviceType");
-		//return "Infrastructure Event";
 	}
 	
 	String getReportedSource() {
 		return getProperties().getString("remedy.reportedSource");
-		//return "Systems Management";
 	}
 
 	String getImpact() {
 		return getProperties().getString("remedy.impact");
-		//return "4-Minor/Localized";
 	}
 
 	String getUrgency() {
 		return getProperties().getString("remedy.urgency");
-		//return "4-Low";
 	}
 	
 	String getResolution() {
 		return getProperties().getString("remedy.resolution");
-		//return "Chiusura da OpenNMS Web Service";
 	}
 
 	String getReOpenStatusReason() {
 		return getProperties().getString("remedy.reason.reopen");
-		//return "Pending Original Incident";
 	}
 	
 	String getResolvedStatusReason() {
 		return getProperties().getString("remedy.reason.resolved");
-		//return "Automated Resolution Reported";
 	}
 	
 	String getCancelledStatusReason() {
 		return getProperties().getString("remedy.reason.cancelled");
-		//return "No longer a Causal CI";
 	}	
 }
