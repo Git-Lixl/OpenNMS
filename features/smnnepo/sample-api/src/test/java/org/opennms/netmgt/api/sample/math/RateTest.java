d9de4659e8702788ef86626fd5db71f332a412f0
Eric Evans <eevans@sym-link.com>
2013-05-21 13:34:55 -0500

    test that NaN samples are written when results are missing

--- features/smnnepo/sample-api/src/test/java/org/opennms/netmgt/api/sample/math/RateTest.java
+++ features/smnnepo/sample-api/src/test/java/org/opennms/netmgt/api/sample/math/RateTest.java
@@ -3,8 +3,10 @@ package org.opennms.netmgt.api.sample.math;
 import static org.junit.Assert.*;
 
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
@@ -14,11 +16,80 @@ import org.opennms.netmgt.api.sample.MetricType;
 import org.opennms.netmgt.api.sample.Resource;
 import org.opennms.netmgt.api.sample.Results;
 import org.opennms.netmgt.api.sample.Sample;
+import org.opennms.netmgt.api.sample.SampleProcessor;
 import org.opennms.netmgt.api.sample.SampleProcessorBuilder;
 import org.opennms.netmgt.api.sample.Timestamp;
+import org.opennms.netmgt.api.sample.Results.Row;
 
 
 public class RateTest extends Util {
+
+	@Test
+	public void testNanSamples() {
+		Agent agent = new Agent(new InetSocketAddress("127.0.0.1", 161), "SNMP", "localhost");
+		final Resource resource = new Resource(agent, "type", "name");
+		final Timestamp ts = Timestamp.now();
+		final Metric metric = new Metric("ifInOctets", MetricType.COUNTER, "mib2-interfaces");
+		final Metric unsampled = new Metric("ifOutOctets", MetricType.COUNTER, "mib2-interfaces");
+
+		@SuppressWarnings("serial")
+		SampleProcessor entrance = new SampleProcessor() {
+			// Two rows with one sample each, for one metric
+			List<Row> rows = new ArrayList<Row>() {{
+				Row r = new Row(resource, ts);
+				r.addSample(new Sample(resource, metric, r.getTimestamp(), 0.0d));
+				add(r);
+
+				r = new Row(resource, ts.plus(300, TimeUnit.SECONDS));
+				r.addSample(new Sample(resource, metric, r.getTimestamp(), 300.0d));
+				add(r);
+			}};
+
+			Iterator<Row> rowsIter = rows.iterator();
+
+			// getMetrics() returns a metric for which no samples exist (ifOutOctets)
+			@Override
+			public Collection<Metric> getMetrics() {
+				return new ArrayList<Metric>() {{
+					add(metric);
+					add(unsampled);
+				}};
+			}
+
+			@Override
+			public Row next() {
+				return rowsIter.next();
+			}
+
+			@Override
+			public boolean hasNext() {
+				return rowsIter.hasNext();
+			}
+		};
+
+		SampleProcessorBuilder chain = new SampleProcessorBuilder().append(entrance).append(new Rate());
+		Iterator<Row> results = chain.getProcessor();
+
+		Row test = results.next();
+
+		/*
+		 * Expected: ifInOctets (metric) should be NaN in the first row (the
+		 * first row is always NaN with Rate), and 1.0 in the second
+		 * ((300.0 / 300s) = 1); ifOutOctets (unsampled) should be present in
+		 * both rows, but NaN.
+		 */
+
+		assertEquals(Double.NaN, test.getSample(metric).getValue(), 0.0d);
+		assertEquals(Double.NaN, test.getSample(unsampled).getValue(), 0.0d);
+
+		test = results.next();
+
+		assertEquals(1.0d, test.getSample(metric).getValue(), 0.0d);
+		assertEquals(Double.NaN, test.getSample(unsampled).getValue(), 0.0d);
+
+		// Iterator should be dry at this point.
+		assertTrue(!results.hasNext());
+	}
 
 	@Test
 	public void test() {
