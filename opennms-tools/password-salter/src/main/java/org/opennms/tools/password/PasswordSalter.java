28482e22953a3bcc2e94839412766dedfcbcc875
Jeff Gehlbach <jeffg@opennms.org>
2015-04-20 12:34:16 -0400

    Stupid tool for salting passwords from the command line

---
+++ opennms-tools/password-salter/src/main/java/org/opennms/tools/password/PasswordSalter.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2015 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2015 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.tools.password;
+
+import org.jasypt.util.password.PasswordEncryptor;
+import org.jasypt.util.password.StrongPasswordEncryptor;
+
+public class PasswordSalter {
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.out.println("You must specify a password on the command line.");
+			System.exit(1);
+		}
+		PasswordEncryptor pe = new StrongPasswordEncryptor();
+		System.out.println(pe.encryptPassword(args[0]));
+	}
+}
